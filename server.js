056530b4f5419c6ae51fe062790774bb50fcaeae
RahulChoudharyGitREPO <132332666+RahulChoudharyGitREPO@users.noreply.github.com>
2025-03-25 22:36:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors"; // ✅ Import CORS
-import dotenv from "dotenv";
 import { userRegister } from "./controllers/userController.js";
 
-dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT =  8000;
 const app = express();
 
 // ✅ Middleware
@@ -15,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ✅ MongoDB Connection (Use .env for security)
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(mongodb+srv://rahulrajwwe2:NmSAs0z1azDDvRwm@cluster0.jcrzs.mongodb.net/, {
     dbname: "NodeJs_Mastery_course"
 }).then(() => {
     console.log("✅ Connected to Database");
@@ -26,8 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // ✅ Routes
 app.post("/users", userRegister);
 
-// ✅ If you are using EJS for rendering views
-app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
     res.render("index");
